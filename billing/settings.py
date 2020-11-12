4bc253d593d1fffd9e2fdf961c7ad048c1e6a0e3
rajesh-dnd <60734842+rajesh-dnd@users.noreply.github.com>
2020-11-12 11:08:54 +0530

    modified db connection

--- billing/settings.py
+++ billing/settings.py
@@ -90,8 +90,7 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
